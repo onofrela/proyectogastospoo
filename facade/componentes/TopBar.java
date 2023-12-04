19bd9a31f3dc6f958d4a8b34e15c80e3f4e3191d
Leonardo Arroyo Onofre <arroyoonofrel@gmail.com>
2023-12-04 17:54:24 -0600

    Correción fuente

--- facade/componentes/TopBar.java
+++ facade/componentes/TopBar.java
@@ -2,6 +2,7 @@ package facade.componentes;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
